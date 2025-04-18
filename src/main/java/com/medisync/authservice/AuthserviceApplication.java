a3e6ecbcf0a7906718dd52867fd10313fba36a8d
Osadha <oshadha_m@epiclanka.net>
2025-04-18 17:51:45 +0530

    Update AuthserviceApplication.java

--- src/main/java/com/medisync/authservice/AuthserviceApplication.java
+++ src/main/java/com/medisync/authservice/AuthserviceApplication.java
@@ -3,6 +3,7 @@ package com.medisync.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//AuthserviceApplication
 @SpringBootApplication
 public class AuthserviceApplication {
 	public static void main(String[] args) {
