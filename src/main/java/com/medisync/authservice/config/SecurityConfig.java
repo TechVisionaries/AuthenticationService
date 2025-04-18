7ff3ee898aa5ba21edcc74de7a2535f4332f5fb7
Osadha <oshadha_m@epiclanka.net>
2025-04-18 18:08:20 +0530

    Update SecurityConfig.java

--- src/main/java/com/medisync/authservice/config/SecurityConfig.java
+++ src/main/java/com/medisync/authservice/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+//security configuration 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
