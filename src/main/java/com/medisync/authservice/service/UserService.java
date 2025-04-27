4a7c2b1e5bf5633540aafa8be0036378e48b00af
Osadha <it21802058@my.sliit.lk>
2025-04-27 15:00:16 +0530

    new APIs added

--- src/main/java/com/medisync/authservice/service/UserService.java
+++ src/main/java/com/medisync/authservice/service/UserService.java
@@ -5,9 +5,15 @@ import com.medisync.authservice.dto.LoginDTO;
 import com.medisync.authservice.dto.UserDTO;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.util.List;
+
 //userService interface
 public interface UserService {
     ApiResponseDTO registerUser(UserDTO userDTO);
 
     ApiResponseDTO authenticateUser(LoginDTO loginDTO, HttpServletResponse response);
+
+    UserDTO findByEmail(String email);
+
+    List<UserDTO> getAllUsers();
 }
